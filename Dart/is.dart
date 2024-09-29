47691872184c60cae8015326e653348cb60610e8
tuhin <hossaintuhin6666@gmail.com>
2024-09-29 23:06:10 +0530

    if and else

--- Dart/is.dart
+++ Dart/is.dart
@@ -3,4 +3,11 @@ void main(){
   if(var1 >5){
     print('this name Raiyada Mahafuj');
   }
+
+  int value = 29;
+  if(value>30){
+    print('this number is true');
+  }else{
+    print('this number is not true');
+  }
 }
